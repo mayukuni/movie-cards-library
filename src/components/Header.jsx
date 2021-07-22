7f9053b7106226473e1c7d71ab5dd89734cb72c6
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-07-22 15:15:13 -0300

    requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,16 @@
 // implement Header component here
 //
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>oi</div>
+    );
+  }
+}
+
+// Header.protoTypes = PropTypes.string.isRequired;
+
+export default Header;
