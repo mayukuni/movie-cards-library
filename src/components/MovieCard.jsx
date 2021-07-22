5981d0ae8defdb779b24a15f1f7052e92aedf9a5
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-07-22 16:07:57 -0300

    requisito 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title } = this.props;
+    return (
+      <section>
+        <h1>
+          {title}
+        </h1>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 // import PropTypes from 'prop-types';
+
 class MovieList extends React.Component {
   render() {
     const {movies} = this.props;
     return (
       <div>
-        oi
+        { movies.map(({title}) =>(
+          <MovieCard key={ title } />
+        ))}
       </div>
+      // <MovieCard key={teste}/>
+      // <MovieCard key={yeah}/>
     );
   }
 }
